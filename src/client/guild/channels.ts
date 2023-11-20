5a96cf1bf8b8af83e783a7ddf46456b4b60dd013
bremea <bremeagames@gmail.com>
2023-11-20 13:19:54 -0500

    Guild channel endpoints

---
+++ src/client/guild/channels.ts
@@ -0,0 +1,39 @@
+import { addAuditLogReason } from "../../utils/helpers";
+import EndpointGroup from "../base";
+import * as ApiTypes from "discord-api-types/v10";
+
+/**	# Guild Channels
+ * 	Group of `/guild/{guild.id}/channel`-related endpoints [Discord Docs](https://discord.com/developers/docs/resources/guild#get-guild-channels)
+ */
+export default class GuildChannels extends EndpointGroup {
+  /**	## Get Guild Channels
+   * 	Returns a list of all guild channel objects. Does not include threads. [Discord Docs](https://discord.com/developers/docs/resources/guild#get-guild-channels)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild to get channels from
+   *	@returns {Promise<ApiTypes.RESTGetAPIGuildChannelsResult>}
+   */
+  async get(guildID: ApiTypes.Snowflake): Promise<ApiTypes.RESTGetAPIGuildChannelsResult> {
+    return await this.client.request<ApiTypes.RESTGetAPIGuildChannelsResult>("GET", `/guilds/${guildID}/channels`);
+  }
+  /**	## Create Guild Channel
+   * 	Create a new channel object for the guild. Requires the `MANAGE_CHANNELS` permission. Returns the new channel object on success. Fires a Channel Create Gateway event. [Discord Docs](https://discord.com/developers/docs/resources/guild#create-guild-channel)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild to create channel in
+   *    @param {ApiTypes.RESTPostAPIGuildChannelJSONBody} options Channel options
+   *    @param {string?} reason Optional reason to include in the guild's audit log
+   *	@returns {Promise<ApiTypes.RESTPostAPIGuildChannelResult>}
+   */
+  async create(guildID: ApiTypes.Snowflake, options: ApiTypes.RESTPostAPIGuildChannelJSONBody, reason?: string): Promise<ApiTypes.RESTPostAPIGuildChannelResult> {
+    return await this.client.request<ApiTypes.RESTPostAPIGuildChannelResult>("POST", `/guilds/${guildID}/channels`, { body: JSON.stringify(options), headers: addAuditLogReason(reason) });
+  }
+  /**	## Modify Guild Channel Positions
+   * 	Modify the positions of a set of channel objects for the guild. Requires MANAGE_CHANNELS permission. Returns a 204 empty response on success. Fires multiple Channel Update Gateway events. [Discord Docs](https://discord.com/developers/docs/resources/guild#modify-guild-channel-positions)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild with the channel(s)
+   *    @param {ApiTypes.RESTPatchAPIGuildChannelPositionsJSONBody} options Array of movement options
+   *	@returns {Promise<ApiTypes.RESTPatchAPIGuildChannelPositionsResult>}
+   */
+  async modifyPositions(guildID: ApiTypes.Snowflake, options: ApiTypes.RESTPatchAPIGuildChannelPositionsJSONBody): Promise<ApiTypes.RESTPatchAPIGuildChannelPositionsResult> {
+    return await this.client.request<ApiTypes.RESTPatchAPIGuildChannelPositionsResult>("PATCH", `/guilds/${guildID}/channels`, { body: JSON.stringify(options) });
+  }
+}
--- src/client/guild/index.ts
+++ src/client/guild/index.ts
@@ -2,17 +2,20 @@ import * as ApiTypes from "discord-api-types/v10";
 import RestClient from "../rest";
 import EndpointGroup from "../base";
 import AuditLogEndpoints from "./auditlog";
-import { serializeIntoQuery } from "../../utils/helpers";
+import GuildChannels from "./channels";
+import { addAuditLogReason, serializeIntoQuery } from "../../utils/helpers";
 
 /**	# Guild Endpoints
  * 	Guilds in Discord represent an isolated collection of users and channels, and are often referred to as "servers" in the UI. [Discord Docs](https://discord.com/developers/docs/resources/guild)
  */
 export default class GuildEndpoints extends EndpointGroup {
   public auditLog: AuditLogEndpoints;
+  public channels: GuildChannels;
 
   constructor(client: RestClient) {
     super(client);
     this.auditLog = new AuditLogEndpoints(client);
+    this.channels = new GuildChannels(client);
   }
 
   /**	## Create Guild
@@ -33,5 +36,43 @@ export default class GuildEndpoints extends EndpointGroup {
    */
   async get(guildID: ApiTypes.Snowflake, withCounts?: boolean): Promise<ApiTypes.RESTGetAPIGuildResult> {
     return await this.client.request<ApiTypes.RESTGetAPIGuildResult>("GET", `/guilds/${guildID}${withCounts ? serializeIntoQuery({ with_counts: withCounts } as ApiTypes.RESTGetAPIGuildQuery) : ""}`);
+  }
+  /**	## Get Guild Preview
+   * 	Returns the guild preview object for the given id. If the user is not in the guild, then the guild must be discoverable. [Discord Docs](https://discord.com/developers/docs/resources/guild#get-guild-preview)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild to fetch
+   *	@returns {Promise<ApiTypes.RESTGetAPIdeleteGuildPreviewResult>} Guild preview data
+   */
+  async getPreview(guildID: ApiTypes.Snowflake): Promise<ApiTypes.RESTGetAPIGuildPreviewResult> {
+    return await this.client.request<ApiTypes.RESTGetAPIGuildPreviewResult>("GET", `/guilds/${guildID}/preview`);
+  }
+  /**	## Modify guild
+   * 	Modify a guild's settings. Requires the `MANAGE_GUILD` permission. Returns the updated guild object on success. Fires a Guild Update Gateway event. [Discord Docs](https://discord.com/developers/docs/resources/guild#modify-guild)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild to modify
+   *    @param {ApiTypes.RESTPatchAPIGuildJSONBody} options Modifications to apply
+   *    @param {string?} reason Optional reason to include in the guild's audit log
+   *	@returns {Promise<ApiTypes.RESTPatchAPIGuildResult>} Updated guild data
+   */
+  async modify(guildID: ApiTypes.Snowflake, options: ApiTypes.RESTPatchAPIGuildJSONBody, reason?: string): Promise<ApiTypes.RESTPatchAPIGuildResult> {
+    return await this.client.request<ApiTypes.RESTPatchAPIGuildResult>("PATCH", `/guilds/${guildID}`, { body: JSON.stringify(options), headers: addAuditLogReason(reason) });
+  }
+  /**	## Delete guild
+   * 	Delete a guild permanently. User must be owner. Returns 204 No Content on success. Fires a Guild Delete Gateway event. [Discord Docs](https://discord.com/developers/docs/resources/guild#delete-guild)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild to delete
+   *	@returns {Promise<ApiTypes.RESTDeleteAPIGuildResult>}
+   */
+  async delete(guildID: ApiTypes.Snowflake): Promise<ApiTypes.RESTDeleteAPIGuildResult> {
+    return await this.client.request<ApiTypes.RESTDeleteAPIGuildResult>("DELETE", `/guilds/${guildID}`);
+  }
+  /**	## List Active Guild Threads
+   * 	Returns all active threads in the guild, including public and private threads. [Discord Docs](https://discord.com/developers/docs/resources/guild#list-active-guild-threads)
+   *
+   * 	@param {ApiTypes.Snowflake} guildID ID of guild to delete
+   *	@returns {Promise<ApiTypes.RESTGetAPIGuildThreadsResult>}
+   */
+  async listActiveThreads(guildID: ApiTypes.Snowflake): Promise<ApiTypes.RESTGetAPIGuildThreadsResult> {
+    return await this.client.request<ApiTypes.RESTGetAPIGuildThreadsResult>("GET", `/guilds/${guildID}/threads/active`);
   }
 }
